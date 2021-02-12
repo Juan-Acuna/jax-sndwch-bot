9bf0f4193eca1a1a9dc34fa6ef466cab3532eb05
= <unusuario256@hotmail.com>
2021-02-12 00:52:30 -0300

    version 0.1.0 (reproductor de musica integrado. lleno de bugs)

--- src/main/java/xyz/sandwichbot/commands/Musica.java
+++ src/main/java/xyz/sandwichbot/commands/Musica.java
@@ -1,44 +1,81 @@
 package xyz.sandwichbot.commands;
 
+import java.net.URI;
 import java.util.ArrayList;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
 import xyz.sandwichbot.annotations.Category;
 import xyz.sandwichbot.annotations.Command;
+import xyz.sandwichbot.core.AutoHelpCommand;
 import xyz.sandwichbot.main.SandwichBot;
+import xyz.sandwichbot.main.util.Comparador;
+import xyz.sandwichbot.main.util.lavaplayer.GuildMusicManager;
+import xyz.sandwichbot.main.util.lavaplayer.PlayerManager;
 import xyz.sandwichbot.models.InputParameter;
+import xyz.sandwichbot.models.InputParameter.InputParamType;
 
 @Category(desc="Comandos de música. ¿Que?¿Acaso esperabas otra descripción?")
 public class Musica {
 	public static boolean enUso = false;
 	
-	@Command(name="Play",desc="Reproduce música obtenida desde una fuente de internet (por defecto YouTube.com)",alias= {"p","r","reproducir","pl"},enabled=false)
+	@Command(name="Play",desc="Reproduce música obtenida desde una fuente de internet (por defecto YouTube.com)",alias= {"p","r","reproducir","pl"},enabled=true)
 	public static void reproducir(MessageReceivedEvent e, ArrayList<InputParameter> parametros) {
-		//TextChannel txtChannel = e.getTextChannel();
-		Guild guild = e.getGuild();
-		Member m = guild.getMember(SandwichBot.ActualBot().getJDA().getSelfUser());
-		GuildVoiceState vs = m.getVoiceState();
-		if(vs.inVoiceChannel()) {
-			//agrega cancion a la reproduccion
+		boolean autodes=false;
+		int autodesTime =15;
+		String busqueda =null;
+		for(InputParameter p : parametros) {
+			if(p.getType() == InputParamType.Standar) {
+				if(p.getKey().equalsIgnoreCase("autodestruir")){
+					autodes=true;
+					if(!p.getValueAsString().equalsIgnoreCase("none")) {
+						autodesTime = p.getValueAsInt();
+					}
+				}else if(p.getKey().equalsIgnoreCase(AutoHelpCommand.HELP_OPTIONS[0])) {
+					AutoHelpCommand.sendHelp(e.getChannel(), "Play");
+					return;
+				}
+			}else if(p.getType() == InputParamType.Custom){
+				busqueda = p.getValueAsString();
+			}
+		}
+		if(busqueda==null) {
+			e.getChannel().sendMessage("Debe ingresar una busqueda.").queue();
 			return;
 		}
-		if(Musica.enUso) {
-			//no puedo, ya estoy en uso
-			//posiblemente mande a otros bots de musica
-			return;
-		}
-		Member invocador = guild.getMember(e.getAuthor());
-		if(!invocador.getVoiceState().inVoiceChannel()) {
-			//necesita estar en un canal de voz para reproducir musica
-			return;
-		}
+		final TextChannel tChannel = e.getTextChannel();
+		final Guild guild = e.getGuild();
+		final Member self = guild.getMember(SandwichBot.ActualBot().getJDA().getSelfUser());
+		final GuildVoiceState selfVoiceState = self.getVoiceState();
 		
+		
+		
+		final Member member = guild.getMember(e.getAuthor());
+		final GuildVoiceState memberVoiceState = member.getVoiceState();
+		if(!memberVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("primero te tienes que meter a un canal de voz para invocarme, no sea gil manit@").queue();
+			return;
+		}/*
+		if(!selfVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("No estoy en tu canal de voz").queue();
+			return;
+		}*/
+		VoiceChannel vchannel = memberVoiceState.getChannel();
+		if(selfVoiceState.inVoiceChannel() && (!memberVoiceState.getChannel().equals(selfVoiceState.getChannel()))) {
+			tChannel.sendMessage("sorry pero espera tu turno, o puedes venir y pedirme musica (y otras cosas:smirk:)").queue();
+			return;
+		}
 		AudioManager audioManager = guild.getAudioManager();
-		VoiceChannel vchannel = invocador.getVoiceState().getChannel();
-		e.getChannel().sendMessage("voy").queue();
 		audioManager.openAudioConnection(vchannel);
+		if(!isURL(busqueda)) {
+			busqueda = "ytsearch:" + busqueda;
+		}
+		PlayerManager.getInstance().loadAndPlay(tChannel, busqueda);
 	}
 	
 	
@@ -46,8 +83,101 @@ public class Musica {
 		
 	}
 	
-	public static void detener() {
+	@Command(name="Siguiente",desc="Salta a la siguiente canción en la cola actual. Si no quedan canciones, la reproducción se termina.",alias={"sk","saltar","skip"})
+	public static void siguiente(MessageReceivedEvent e, ArrayList<InputParameter> parametros) {
+		final TextChannel tChannel = e.getTextChannel();
+		final Guild guild = e.getGuild();
+		final Member self = guild.getMember(SandwichBot.ActualBot().getJDA().getSelfUser());
+		final GuildVoiceState selfVoiceState = self.getVoiceState();
 		
+		
+		if(!selfVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("No estoy en tu canal de voz").queue();
+			return;
+		}
+		final Member member = guild.getMember(e.getAuthor());
+		final GuildVoiceState memberVoiceState = member.getVoiceState();
+		
+		if(!memberVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("conectate a un canal de voz poh sacowea").queue();
+			return;
+		}
+		if(!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+			tChannel.sendMessage("tienes que estar conmigo en el canal de voz para pedirme musica (y otras cosas:smirk:)").queue();
+			return;
+		}
+		final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(guild);
+		final AudioPlayer player = musicManager.audioPlayer;
+		
+		if(player.getPlayingTrack() == null) {
+			tChannel.sendMessage("No se esta reproduciendo ninguna canción.").queue();
+			return;
+		}
+		tChannel.sendMessage("Saltando...").queue();
+		musicManager.scheduler.nextTrack();
+	}
+	
+	@Command(name="Detener",desc="Detiene la reproducción actual.",alias={"stop","det"})
+	public static void detener(MessageReceivedEvent e, ArrayList<InputParameter> parametros) {
+		final TextChannel tChannel = e.getTextChannel();
+		final Guild guild = e.getGuild();
+		final Member self = guild.getMember(SandwichBot.ActualBot().getJDA().getSelfUser());
+		final GuildVoiceState selfVoiceState = self.getVoiceState();
+		
+		
+		if(!selfVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("No estoy en tu canal de voz").queue();
+			return;
+		}
+		final Member member = guild.getMember(e.getAuthor());
+		final GuildVoiceState memberVoiceState = member.getVoiceState();
+		if(!memberVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("conectate a un canal de voz poh sacowea").queue();
+			return;
+		}
+		if(!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+			tChannel.sendMessage("tienes que estar conmigo en el canal de voz para pedirme musica (y otras cosas:smirk:)").queue();
+			return;
+		}
+		
+		final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(guild);
+		musicManager.scheduler.player.stopTrack();
+		musicManager.scheduler.queue.clear();
+		tChannel.sendMessage("Canción detenida (y lista de reproduccion eliminada.)").queue();
+	}
+	
+	@Command(name="Actual",desc="Indica la canción que se esta reproduciendo actualmente.",alias= {"np","playing","cancion"})
+	public static void actual(MessageReceivedEvent e, ArrayList<InputParameter> parametros) {
+		final TextChannel tChannel = e.getTextChannel();
+		final Guild guild = e.getGuild();
+		final Member self = guild.getMember(SandwichBot.ActualBot().getJDA().getSelfUser());
+		final GuildVoiceState selfVoiceState = self.getVoiceState();
+		
+		
+		if(!selfVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("No estoy en tu canal de voz").queue();
+			return;
+		}
+		final Member member = guild.getMember(e.getAuthor());
+		final GuildVoiceState memberVoiceState = member.getVoiceState();
+		
+		if(!memberVoiceState.inVoiceChannel()) {
+			tChannel.sendMessage("conectate a un canal de voz poh sacowea").queue();
+			return;
+		}
+		if(!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+			tChannel.sendMessage("tienes que estar conmigo en el canal de voz para pedirme musica (y otras cosas:smirk:)").queue();
+			return;
+		}
+		final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(guild);
+		final AudioPlayer player = musicManager.audioPlayer;
+		final AudioTrack track = player.getPlayingTrack();
+		if(track == null) {
+			tChannel.sendMessage("No se esta reproduciendo ninguna canción.").queue();
+			return;
+		}
+		final AudioTrackInfo info = track.getInfo();
+		tChannel.sendMessageFormat("Canción actual: %s de %s (%s)",info.title,info.author,info.uri).queue();
 	}
 	
 	public static void buscar() {
@@ -58,4 +188,8 @@ public class Musica {
 		
 	}
 	
+	private static boolean isURL(String txt) {
+		String a = Comparador.Encontrar("http[s]{0,1}://[a-zA-Z0-9%+_-]{1,60}.[a-zA-Z0-9]{2,3}/", txt);
+		return a!=null;
+	}
 }
---
+++ src/main/java/xyz/sandwichbot/main/util/lavaplayer/PlayerManager.java
@@ -0,0 +1,89 @@
+package xyz.sandwichbot.main.util.lavaplayer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+public class PlayerManager {
+	private static PlayerManager _instance;
+	private final Map<Long, GuildMusicManager> musicManagers;
+	private final AudioPlayerManager audioPlayerManager;
+	
+	public PlayerManager() {
+		this.musicManagers = new HashMap<>();
+		this.audioPlayerManager = new DefaultAudioPlayerManager();
+		AudioSourceManagers.registerRemoteSources(audioPlayerManager);
+		AudioSourceManagers.registerLocalSource(audioPlayerManager);
+	}
+	public GuildMusicManager getMusicManager(Guild guild) {
+		return this.musicManagers.computeIfAbsent(guild.getIdLong(), (guildId) -> {
+			final GuildMusicManager guildMusicManager = new GuildMusicManager(this.audioPlayerManager);
+			guild.getAudioManager().setSendingHandler(guildMusicManager.getSendHandler());
+			
+			return guildMusicManager;
+		});
+	}
+	public static PlayerManager getInstance() {
+		if(_instance==null) {
+			return new PlayerManager();
+		}
+		return _instance;
+	}
+	public void loadAndPlay(TextChannel channel, String trackUrl){
+		final GuildMusicManager musicManager = this.getMusicManager(channel.getGuild());
+		this.audioPlayerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
+
+			@Override
+			public void trackLoaded(AudioTrack track) {
+				musicManager.scheduler.queue(track);
+				
+				channel.sendMessage("AGREGANDO A LA COLA: *")
+				.append(track.getInfo().title)
+				.append("* de *")
+				.append(track.getInfo().author + "*")
+				.queue();
+			}
+
+			@Override
+			public void playlistLoaded(AudioPlaylist playlist) {
+				final List<AudioTrack> tracks = playlist.getTracks();
+				if(playlist.isSearchResult()) {
+					trackLoaded(tracks.get(0));
+					return;
+				}
+				channel.sendMessage("AGREGANDO A LA COLA: *")
+				.append(String.valueOf(tracks.size()))
+				.append("* canciones de la lista *")
+				.append(playlist.getName() + "*")
+				.queue();
+				for(final AudioTrack track : tracks) {
+					musicManager.scheduler.queue(track);
+				}
+			}
+
+			@Override
+			public void noMatches() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void loadFailed(FriendlyException exception) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	}
+}
