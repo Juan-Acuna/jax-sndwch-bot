b4e6647ed3348a9d6b224b193df99446f9fee4c4
= <=>
2021-09-27 15:36:52 -0300

    correcciones varias, v0.7.2

--- src/main/java/com/jaxsandwich/discordbot/main/SandwichBot.java
+++ src/main/java/com/jaxsandwich/discordbot/main/SandwichBot.java
@@ -78,12 +78,12 @@ public class SandwichBot extends Bot{
 			this.getGuildConfigManager().loadData(l);
 			FuenteImagen.load(CommandManager.selectAll(FuenteImagen.class));
 			Fuente.load(CommandManager.selectAll(Fuente.class));
-			for(CommandObject cmd : CommandObject.getAsList()) {
+			/*for(CommandObject cmd : CommandObject.getAsList()) {
 				System.out.print("\nComando: "+cmd.getName(def_lang));
 				for(String alias : cmd.getAlias(def_lang)) {
 					System.out.print("["+alias+"]");
 				}
-			}
+			}*/
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
